9bd32d9154beb76e713149326c22ac5f388b7075
massive2351 <massive2351@gmail.com>
2021-04-05 05:51:50 +0000

    finish いいね非同期通信

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,17 +1,23 @@
 class FavoritesController < ApplicationController
   before_action :authenticate_user!
+  before_action :book_params
 
   def create
-    @book = Book.find(params[:book_id])
-    favorite = @book.favorites.new(user_id: current_user.id)
+    book = Book.find(params[:book_id])
+    favorite = current_user.favorites.new(book_id: book.id)
     favorite.save
-    redirect_to request.referer
+    
   end
 
   def destroy
-    @book = Book.find(params[:book_id])
-    favorite = @book.favorites.find_by(user_id: current_user.id)
+    book = Book.find(params[:book_id])
+    favorite = current_user.favorites.find_by(book_id: book.id)
     favorite.destroy
-    redirect_to request.referer
+  end
+  
+  private
+
+  def book_params
+      @book= Book.find(params[:book_id])
   end
 end
